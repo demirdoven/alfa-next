6dfc3066d720372e84efbff178796460ca253ae7
Selman Demirdoven <sdemirdoven35@gmail.com>
2024-02-09 03:02:49 +0300

    order-received

--- app/order-received/page.js
+++ app/order-received/page.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default async function OrderReceived( {searchParams} ){
 
     console.log(searchParams.order_id)
-  return (
-    <div>OrderReceived</div>
-  )
+    return (
+        <div className='w-full'>
+
+            <h1 className='text-lg text-center'>THANK YOU</h1>
+
+        </div>
+    )
 }
