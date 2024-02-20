c628db8cfeaaf6153452dd5fff7a1aa55156ffb3
Selman Demirdoven <sdemirdoven35@gmail.com>
2024-02-20 15:43:14 +0100

    test pid

--- app/api/create-order/route.js
+++ app/api/create-order/route.js
@@ -4,7 +4,7 @@ import { isEmpty } from 'lodash';
 const WooCommerceRestApi = require( '@woocommerce/woocommerce-rest-api' ).default;
 
 const api = new WooCommerceRestApi( {
-	url: process.env.ALFA_LIVE_URL,
+	url: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL,
 	consumerKey: process.env.ALFA_LIVE_WC_CONSUMER_KEY,
 	consumerSecret: process.env.ALFA_LIVE_WC_CONSUMER_SECRET,
 	version: "wc/v3"
