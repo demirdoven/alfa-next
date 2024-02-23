fed3851abf4a2cbd9e7a9ba2d38dd00093f0f976
Selman Demirdoven <sdemirdoven35@gmail.com>
2024-02-23 18:52:49 +0000

    logo

--- components/header/Logo.js
+++ components/header/Logo.js
@@ -6,13 +6,13 @@ const Logo = ({className}) => {
     <div className={`${className}`}>
         <Link href="/" className="flex justify-center">
             <Image 
-            priority={true} 
-            src="https://alfatires.com/wp-content/uploads/LOGO-1024x394-1.webp" 
-            alt="CoolNomad" 
-            width="0"
-            height="0"
-            sizes="16vw"
-            className="w-auto lg:h-full h-[4rem]"
+              priority={true} 
+              src="/alfatires-logo-de_DE.webp" 
+              alt="Alfatires.com" 
+              width="0"
+              height="0"
+              sizes="16vw"
+              className="w-auto lg:h-full h-[4rem]"
           />
         </Link>
     </div>
