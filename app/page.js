bbabb57970868fcd6642da2f08646de7fe4c2639
Selman Demirdoven <sdemirdoven35@gmail.com>
2024-02-24 18:45:17 +0000

    hero

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ const HomePage = () => {
   return (
     <div className='w-full'>
 
-      <Hero />
+      {/* <Hero /> */}
       <BrandSlider />
       <SchnellAndEinfach />
       <UnsereReifenempfehlung />
