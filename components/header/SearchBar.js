86b6e2d89e25b266b0e3011f251a86be84bd2786
Selman Demirdoven <sdemirdoven35@gmail.com>
2024-02-25 16:25:50 +0000

    search bar

--- components/header/SearchBar.js
+++ components/header/SearchBar.js
@@ -12,13 +12,15 @@ const SearchBar = ( {className} ) => {
     }
     function startTyping(){
         setTypingActive(true) 
+        document.getElementById('s').value = ''
     }
 
     return (
         <div 
-            className={`w-[50%] m-auto relative bg-gray-50 border-[2px] border-slate-600 rounded-lg ${className}`}
+            className={`w-[50%] m-auto relative bg-gray-100 border-[1px] border-slate-600 rounded-lg ${className}`}
         >
             <input 
+                id="s"
                 name=""
                 // value=""
                 className={` h-[40px] relative z-10 bg-transparent ${ typingActive ? 'opacity-0' : 'opacity-100' } text-gray-900 text-xl border border-gray-300 rounded-lg block w-full p-2.5 pl-[10px] outline-0`}
@@ -29,11 +31,10 @@ const SearchBar = ( {className} ) => {
             <TypingLayout stopTyping={stopTyping} /> 
              : '' }
 
-            <button
-                className="absolute z-[200] w-auto h-full top-1/2 transform -translate-y-1/2 right-0 bg-slate-600 text-white hover:bg-slate-800 text-red-600 text-xl font-bold py-1 px-4 border border-slate-600 rounded-r-md uppercase text-center cursor-pointer"
-            >
-                SEARCH
-            </button>
+<button class="absolute z-[200] w-auto h-full top-1/2 transform -translate-y-1/2 right-0 bg-slate-600 text-white hover:bg-slate-800 text-red-600 text-xl font-bold py-2 px-4 border border-slate-600 hover:border-slate-800 rounded-r-md uppercase flex items-center cursor-pointer">
+  SEARCH
+</button>
+
         </div>
     )
 }
