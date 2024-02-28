e65c3b8f0abe24914f864702c9cb67bbdff9dac0
Selman Demirdoven <sdemirdoven35@gmail.com>
2024-02-28 22:14:48 +0000

    misc

--- components/header/StickyBar.js
+++ components/header/StickyBar.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 const StickyBar = () => {
   return (
     <div className="sticky-bar bg-alfa-red-1 text-white text-center text-xs font-normal py-2">
-        <Link href={'/'} className="block px-10">Bis zu 10% Rabatt auf Allwetterreifen von Syron und Berlin Tires / Nur für kurze Zeit!</Link>
+        <Link href={'/'} className="block px-10">10% discount on all-weather tires from Syron and Berlin Tires / Only for a short time!</Link>
     </div>
   )
 }
