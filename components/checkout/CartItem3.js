025c93fc5f23b9b469e06e0078fc9f64cc330c8c
Selman Demirdoven <sdemirdoven35@gmail.com>
2024-02-29 16:47:58 +0000

    mobile mini cart testing

--- components/checkout/CartItem3.js
+++ components/checkout/CartItem3.js
@@ -87,12 +87,36 @@ const CartItem3 = ({item, products, setColor, setUpdatingProduct, setRemovingPro
                     height={30}
                 />
             <div className="flex flex-col justify-between ml-4 flex-grow">
-                {item.data.name}
-            {/* <span className="text-red-500 text-xs">Apple</span> */}
+            {item.data.name}            
+            <span className="text-red-500 text-xs">Apple</span>
             </div>
         </div>
-       
-       
+        <div className="flex flex-col w-2/12">
+            <div className="qty_changer w-full">
+                <div className="flex items-center border-gray-100">
+                    <span 
+                        onClick={( event ) => handleQtyChange( event, item?.cartKey, 'decrement' )}
+                        className="block text-center leading-[1.13rem] cursor-pointer rounded-full bg-white hover:bg-slate-900 text-slate-900 hover:text-white border border-slate-900 w-[20px] h-[20px]"> - </span>
+                    <span 
+                        className="flex items-center justify-center text-md mx-2">
+                    {item.quantity}
+                    </span>
+                    <span 
+                        onClick={( event ) => handleQtyChange( event, item?.cartKey, 'increment' )}
+                        className="block text-center leading-[1.13rem] cursor-pointer rounded-full bg-white hover:bg-slate-900 text-slate-900 hover:text-white border border-slate-900 w-[20px] h-[20px]"> + </span>
+                </div>
+            </div>
+        </div>
+        <div className="flex flex-col  justify-center">
+            <span className="text-center w-2/12 font-normal text-md whitespace-nowrap">{item?.line_subtotal.toFixed(2)} €</span>
+            { item.quantity > 1 && <span className="text-center w-full font-normal text-slate-900 opacity-40 text-xs whitespace-nowrap">{item.quantity} x {item?.data?.price} €</span> }
+        </div>
+        <button 
+            onClick={ ( event ) => handleRemoveProductClick( event, item?.key ) }
+            data-key={item?.key}
+            className="text-center w-1/12 font-normal text-sm hover:text-alfa-red-1" title="Remove">{<RiDeleteBin6Line />}</button>
+
+
         
     
     </div>
