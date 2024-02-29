b9f28a65709e6097c877111b8d23401f81477f4c
Selman Demirdoven <sdemirdoven35@gmail.com>
2024-02-29 14:50:57 +0000

    mobile mini cart test

--- components/checkout/CartItem3.js
+++ components/checkout/CartItem3.js
@@ -17,7 +17,7 @@ const CartItem3 = ({item, products, setColor, setUpdatingProduct, setRemovingPro
 
     <div key={item.key} className="cart-item w-full flex items-center justify-between bg-gray-100 hover:bg-gray-200 mb-2 pl-6 pr-2 py-5 rounded-md">
         <div className="flex w-5/12">
-            <Link href={`/product/adsdsads`} className="w-20">
+            <Link href={`/product/adsdsads`} className="">
                 {/* <Image
                     className="max-h-24 w-auto object-contain object-top"
                     src={item.data?.images[0].src}
