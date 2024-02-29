7d513a51ee14455ad415c3948b6eefc608d7ecc9
Selman Demirdoven <sdemirdoven35@gmail.com>
2024-02-29 18:06:22 +0000

    testingggggggg

--- components/checkout/CartItem3.js
+++ components/checkout/CartItem3.js
@@ -19,9 +19,9 @@ const CartItem3 = ({item, products, setColor, setUpdatingProduct, setRemovingPro
     <div key={item.key} className="cart-item w-full flex items-center justify-between bg-gray-100 hover:bg-gray-200 mb-2 pl-6 pr-2 py-5 rounded-md">
         
         
-        <Link href={`/products/tires`}>
+        
         test
-        </Link>
+       
     
     </div>
 
