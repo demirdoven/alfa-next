660469400f88dbc41766729c0e2e388d7514b70f
Selman Demirdoven <sdemirdoven35@gmail.com>
2024-02-29 14:14:09 +0000

    mobile mini cart test

--- components/checkout/CartItem3.js
+++ components/checkout/CartItem3.js
@@ -1,77 +1,7 @@
 import Link from 'next/link';
 import { useState } from 'react';
-import { RiDeleteBin6Line } from 'react-icons/ri';
-import { isEmpty } from 'lodash';
-import { sepetiGuncelle, sepetUrunSil } from '@/lib/functions';
 
-const CartItem3 = ({item, products, setColor, setUpdatingProduct, setRemovingProduct }) => {
-
-    const [productCount, setProductCount] = useState( item.quantity );
-
-	const productImg = item?.data?.images?.[0] ?? '';
-
-
-    const handleQtyChange = ( event, cartKey, type ) => {
-		
-		if ( process.browser ) {
-			
-			event.stopPropagation();
-
-            setUpdatingProduct(true);
-
-			let newQty;
-			
-			// If the previous cart request is still updatingProduct or removingProduct, then return.
-			// if ( updatingProduct || removingProduct || ( 'decrement' === type && 1 === productCount ) ) {
-			// 	return;
-			// }
-			
-			if ( !isEmpty( type ) ) {
-				newQty = 'increment' === type ? productCount + 1 : productCount - 1;
-			} else {
-				// If the user tries to delete the count of product, set that to 1 by default ( This will not allow him to reduce it less than zero )
-				newQty = ( event.target.value ) ? parseInt( event.target.value ) : 1;
-			}
-			
-            
-			// Set the new qty in state.
-			setProductCount( newQty );
-			
-
-			if ( products.length ) {
-				// updateCart(item?.key, newQty, setCart, setUpdatingProduct);
-				sepetiGuncelle(item?.key, newQty, setColor, setUpdatingProduct );
-			}
-			
-		}
-	};
-	
-    const handleRemoveProductClick = ( event, cartKey ) => {
-		event.stopPropagation();
-		
-		// // If the component is unmounted, or still previous item update request is in process, then return.
-		// if ( !isMounted || updatingProduct ) {
-		// 	return;
-		// }
-		
-        // console.log(event);
-
-        // return false;
-		sepetUrunSil( cartKey, setColor, setRemovingProduct );
-
-        
-
-        // var wrapDiv = event.target.closest('.cart-itemm');
-        // if (wrapDiv) {
-        //   // Remove the parent "wrap" div if it exists
-        //   wrapDiv.parentNode.removeChild(wrapDiv);
-        // }
-
-        event.target.closest('.cart-item').classList.add('zero-height');
-        
-
-      
-	};
+const CartItem3 = ({item }) => {
 
 
     return (
@@ -80,14 +10,10 @@ const CartItem3 = ({item, products, setColor, setUpdatingProduct, setRemovingPro
         <div className="flex w-5/12">
            
             <div className="flex flex-col justify-between ml-4 flex-grow">
-            <Link href={`http://localhost:3000/product/${item?.data?.slug}`} className="font-semibold text-sm">{item.data.name}</Link>
-            {/* <span className="text-red-500 text-xs">Apple</span> */}
+                {item.data.name}
             </div>
         </div>
-        
-        
-        
-    
+
     </div>
 
   )
