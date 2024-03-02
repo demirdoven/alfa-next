f84bff36f0d4d4d3c48b57bc1e882225ac10cf18
Selman Demirdoven <sdemirdoven35@gmail.com>
2024-03-02 02:25:03 +0000

    hero mobile fix

--- components/products/Placeholder.js
+++ components/products/Placeholder.js
@@ -12,15 +12,14 @@ const Placeholder = ({catSlug}) => {
                           <div data-pid={'.'} className="w-full mb-4 pl-5 pr-7 pt-5 pb-7 overflow-hidden rounded-lg border-2 border-gray-200 bg-white hover:shadow-lg">
                               <div className="relative animate-pulse flex space-x-4 relative flex flex-row ">
                               
-                                  <div className="w-3/12 relative mx-3 mt-3 flex flex-col gap-y-2">
-                                      <div className="bg-gray-200 h-36 w-full rounded-lg"></div>
+                                  <div className="w-3/12 relative ml-3 mr-1 lg:mx-3 mt-2 lg:mt-3 flex flex-col gap-y-2">
+                                      <div className="bg-gray-200 h-20 lg:h-36 w-full rounded-lg"></div>
                                       { catSlug == 'tires' && <div className="bg-gray-200 h-10 w-full rounded-xl"></div> }
                                   </div>
                                   
-                                  <div className="relative w-8/12 mt-4 px-5 ">
+                                  <div className="relative w-8/12 mt-2 lg:mt-4 px-3 lg:px-5 ">
                                       <div className="bg-gray-200 h-5 w-full mb-6 rounded-lg"></div>
                                       <div className=' w-full '>
-                                          <div className="bg-gray-200 h-3 w-4/12 mb-2  rounded-lg"></div>
                                           <div className="bg-gray-200 h-3 w-4/12 mb-2  rounded-lg"></div>
                                           <div className="bg-gray-200 h-3 w-4/12 mb-2  rounded-lg"></div>
                                       </div>
@@ -29,7 +28,7 @@ const Placeholder = ({catSlug}) => {
                                   <div className="w-3/12 pb-1 pr-1 flex flex-col align-center justify-end	">
                                       
                                       
-                                      <span className="bg-gray-200 h-10 w-full rounded-lg"></span>
+                                      <span className="bg-gray-200 h-8 lg:h-10 w-full rounded-lg"></span>
           
           
           
