114371af0c51b080448efbb987b94c68e866b6a1
Selman Demirdoven <sdemirdoven35@gmail.com>
2024-05-03 19:10:25 +0300

    catslug test

--- app/products/[catSlug]/page.js
+++ app/products/[catSlug]/page.js
@@ -1,25 +1,26 @@
-import Main from "@/components/products/Main";
-import { isEmpty } from "lodash";
-import { headers } from 'next/headers'
+// import Main from "@/components/products/Main";
+// import { isEmpty } from "lodash";
+// import { headers } from 'next/headers'
 
-const getDeviceType = () => {
-    const headersList = headers();
-    const userAgent = headersList.get('user-agent');
+// const getDeviceType = () => {
+//     const headersList = headers();
+//     const userAgent = headersList.get('user-agent');
   
-    return userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i)
-      ? 'mobile'
-      : 'desktop';
-}
+//     return userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i)
+//       ? 'mobile'
+//       : 'desktop';
+// }
 
 export default async function catSlug( {params, searchParams} ){
 
-    const device = getDeviceType()
+    // const device = getDeviceType()
 
     const catSlug = params.catSlug;
 
-    return (
-        <div className="w-full xs:px-4 lg:container mx-auto lg:max-w-6xl mt-6 px-4">
-            <Main catSlug={catSlug} searchParams={searchParams} device={device} />
-        </div>
-    )
+    return 'selam';
+    // return (
+    //     <div className="w-full xs:px-4 lg:container mx-auto lg:max-w-6xl mt-6 px-4">
+    //         <Main catSlug={catSlug} searchParams={searchParams} device={device} />
+    //     </div>
+    // )
 }
